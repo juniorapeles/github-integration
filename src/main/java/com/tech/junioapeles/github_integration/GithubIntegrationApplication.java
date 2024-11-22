329a9d80919d47724736edc85b8d65462b991923
juniorapeles <elijunior.apeles2005@gmail.com>
2024-11-21 21:11:15 -0300

    feat: first commit

---
+++ src/main/java/com/tech/junioapeles/github_integration/GithubIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.tech.junioapeles.github_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/junioapeles/github_integration/GithubIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.junioapeles.github_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
